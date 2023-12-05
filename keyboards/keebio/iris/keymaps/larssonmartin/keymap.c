9b75533bf64d261b1e037919d9458298ccb99226
Martin Larsson <larssonmartin1998@gmail.com>
2023-12-05 23:46:14 +0100

    Turn off retro tapping, change tapping term to 170. Tweak layout to move away from the fourth number row. Still need to figure out how to get rid of one thumb key to transition to corne

--- keyboards/keebio/iris/keymaps/larssonmartin/keymap.c
+++ keyboards/keebio/iris/keymaps/larssonmartin/keymap.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 #include "action_layer.h"
+#include "action_tapping.h"
+#include "config.h"
 #include "keyboard.h"
 #include "keycodes.h"
 #include "keymap_us.h"
@@ -8,13 +10,25 @@
 #include "rgb_matrix.h"
 #include QMK_KEYBOARD_H
 
-enum custom_layer { MAIN, MAIN_LIN, FN, SYMBOLS, NUM_LAYERS };
+enum custom_layer { MAIN, MAIN_LIN, NUMBERS, SYMBOLS, NUM_LAYERS };
 
 #define MT_CLEC LCTL_T(KC_ESC)
 #define MT_CLQT RCTL_T(KC_QUOT)
-#define MO_FN MO(FN)
-#define MO_SM MO(SYMBOLS)
+
+#define MT_LSTG LSFT_T(CW_TOGG)
+#define MT_RSTG RSFT_T(CW_TOGG)
+
+#define MO_NUM MO(NUMBERS)
+#define MO_SYM MO(SYMBOLS)
+
 #define TG_LIN TG(MAIN_LIN)
+
+// These doesn't work, need to implement them specially.
+// There is a limitation in QMK's Mod-Tap: https://docs.qmk.fm/#/mod_tap
+// #define LGUI_TOG LGUI_T(CW_TOGG)
+// #define RGUI_TOG RGUI_T(CW_TOGG)
+// #define LALT_TOG LALT_T(CW_TOGG)
+// #define RALT_TOG RALT_T(CW_TOGG)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -28,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LALT,          KC_RALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LGUI, MO_FN,  KC_SPC,                    KC_ENT,  MO_SM,  KC_RGUI
+                                    KC_LGUI, MO_NUM,  KC_SPC,                    KC_ENT,  MO_SYM,  KC_RGUI
   ),
 
   [MAIN_LIN] = LAYOUT(
@@ -41,18 +55,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LGUI,          KC_RGUI, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LALT, MO_FN,  KC_SPC,                    KC_ENT,  MO_SM,  KC_RALT
+                                    KC_LALT, KC_LALT, KC_SPC,                    KC_ENT, KC_RALT,  KC_RALT
   ),
 
-  [FN] = LAYOUT(
+  [NUMBERS] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, KC_DEL,
+     KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, _______, _______, _______, _______, _______,                            KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, QK_BOOT,
+     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,          _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    TG_LIN,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -60,13 +74,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [SYMBOLS] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+     _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_QUOT, KC_BSLS, KC_MINS, KC_PLUS, _______,                            _______, KC_EQL,  KC_UNDS, KC_PIPE, KC_DQUO, _______,
+     _______, KC_QUOT, KC_BSLS, KC_MINS, KC_PLUS, _______,                            _______, KC_EQL,  KC_UNDS, KC_PIPE, KC_DQUO, KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_LBRC, KC_LT,   KC_LPRN, KC_LCBR, _______,                            _______, KC_RCBR, KC_RPRN, KC_GT,   KC_RBRC, _______,
+     KC_TILD, KC_LBRC, KC_LT,   KC_LPRN, KC_LCBR, _______,                            _______, KC_RCBR, KC_RPRN, KC_GT,   KC_RBRC, KC_GRV,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, _______, _______, _______, _______, _______,          _______, _______, TG_LIN,  _______, _______, _______, _______,
+     _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______,          _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_HOME, KC_END,  QK_BOOT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
