f1b2b282844f1341067d78153797730126088399
Martin Larsson <larssonmartin1998@gmail.com>
2024-05-18 10:25:54 +0200

    Slightly increase the tapping term form 175 to 182

--- keyboards/keebio/iris/rev8/config.h
+++ keyboards/keebio/iris/rev8/config.h
@@ -33,7 +33,7 @@
 // Mod-tap
 // #define RETRO_TAPPING
 #define TAPPING_TERM_PER_KEY
-#define TAPPING_TERM 175
+#define TAPPING_TERM 182
 // #define IGNORE_MOD_TAP_INTERRUPT /* for rolling on mod-tap keys */
 #define BILATERAL_COMBINATIONS
 #define BILATERAL_COMBINATIONS_LIMIT_CHORD_TO_N_KEYS 4 /* GUI, Alt, Ctrl, Shift */
